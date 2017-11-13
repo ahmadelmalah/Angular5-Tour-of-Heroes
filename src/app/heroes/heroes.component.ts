e9bd15aec49d7ca5d540fb78c0880ccf9e9f4c6f
Ahmed Elmalah <ahmedelmalah@hotmail.com>
2017-11-13 21:08:30 +0200

    Displaying Heroes from its array

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -18,4 +19,6 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes = HEROES;
 }
