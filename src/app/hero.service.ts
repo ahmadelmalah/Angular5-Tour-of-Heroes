1fc3b82cdd0e3ef06351aa6eb8da905c79a798be
Ahmed Elmalah <ahmedelmalah@hotmail.com>
2017-11-22 10:37:42 +0200

    Getting data from http server instead of mock data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,14 +11,15 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';  // URL to web api
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    // Todo: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
